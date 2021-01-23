97de236cc80e4af9c668f6816b9eee27c979769e
Samuel Štancl <samuel.stancl@gmail.com>
2021-01-23 21:29:02 +0100

    Initial commit

---
+++ src/PackageServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Stancl\Package;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+        // $this->loadViewsFrom(__DIR__ . '/../assets/views', 'package');
+
+        // $this->publishes([
+        //     __DIR__ . '/../assets/views' => resource_path('views/vendor/package'),
+        // ], 'package-views');
+
+        // $this->mergeConfigFrom(
+        //     __DIR__ . '/../assets/package.php',
+        //     'package'
+        // );
+
+        // $this->publishes([
+        //     __DIR__ . '/../assets/package.php' => config_path('package.php'),
+        // ], 'package-config');
+    }
+}
